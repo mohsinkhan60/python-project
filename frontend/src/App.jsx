b2f5e7ecb19d26d9b664a652ce611570824ca6ea
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-02-19 00:50:56 +0500

    feat: implement contacts creation feature with modal form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,45 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ContactsList from "./ContactsList";
+import ContactsForm from "./ContactsForm";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [isModelOpen, setIsModelOpen] = useState(false);
 
   useEffect(() => {
-    fatchContacts()
-  })
+    fatchContacts();
+  });
 
   const fatchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
-  }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
 
-  return <div>
-    <ContactsList contacts={contacts} />
-  </div>;
+  const closeModel = () => {
+    setIsModelOpen(false);
+  };
+
+  const openCreateModel = () => {
+    setIsModelOpen(true);
+  };
+
+  return (
+    <div>
+      <ContactsList contacts={contacts} />
+      <button onClick={openCreateModel}>Create New Contact</button>
+      {isModelOpen && (
+        <div className="model">
+          <div className="model-content">
+            <span className="close" onClick={closeModel}>close</span>
+            <ContactsForm />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
