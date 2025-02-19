5f9780d0b5d3bc37012c584c7d08612beaa951e6
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-02-19 14:14:05 +0500

    feat: add delete functionality to contacts list with API integration

--- frontend/src/ContactsList.jsx
+++ frontend/src/ContactsList.jsx
@@ -3,6 +3,21 @@
 
 
 const ContactsList = ({contacts, updateContact, updateCallBack}) => {
+   const deleteContact = async (id) => {
+      try {
+         const options = {
+            method: "DELETE"
+         }
+         const response = await fetch (`http://127.0.0.1:5000/delete_contact/${id}`, options)
+         if (response.status === 200){
+            updateCallBack()
+         }else{
+            console.error("Failed to delete")
+         }
+      } catch (error) {
+         alert(error)
+      }
+   }
   return (
     <div>
       <h2>Contacts</h2>
@@ -23,7 +38,7 @@ const ContactsList = ({contacts, updateContact, updateCallBack}) => {
                   <td>{contact.email}</td>
                   <td>
                      <button onClick={() => updateContact(contact)}>update</button>
-                     <button>delete</button>
+                     <button onClick={() => deleteContact(contact.id)}>delete</button>
                   </td>
                </tr>
             ))}
